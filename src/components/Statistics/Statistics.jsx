340dee261622c3c6bf8e4843b05eaf4f50cd045b
PavelBazilevich <102550003+PavelBazilevich@users.noreply.github.com>
2022-10-15 09:34:45 +0300

    upgreade

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -6,11 +6,11 @@ class Statistics extends Component {
       <div>
         <h2>Statistics</h2>
         <ul>
-          <li>Good:{this.props.state.good}</li>
-          <li>Neutral:{this.props.state.neutral}</li>
-          <li>Bad:{this.props.state.bad}</li>
-          <li>Total:{this.props.countTotalFeedback}</li>
-          <li>Positive feetback:{this.props.percentage}%</li>
+          <li>Good: {this.props.state.good}</li>
+          <li>Neutral: {this.props.state.neutral}</li>
+          <li>Bad: {this.props.state.bad}</li>
+          <li>Total: {this.props.countTotalFeedback}</li>
+          <li>Positive feetback: {this.props.percentage}%</li>
         </ul>
       </div>
     );
